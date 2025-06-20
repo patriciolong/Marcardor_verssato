71bef5dd85443a26fb9c72040356e52b679ac801
patriciolong <90225154+patriciolong@users.noreply.github.com>
2025-06-20 14:20:24 -0500

    Delete Public_html directory

--- Public_html/controlers_carpet/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "clockin_db2";
-
-$conexion = new mysqli($server, $user, $pass, $db);
-?>
--- Public_html/includes/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date('Y'); ?> ClockIn. Todos los derechos reservados.</p>
-    </footer>
-</body>
-</html>
